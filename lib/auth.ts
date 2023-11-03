6d540a75bbcc7aa7c4471e3b52a98e070c8cf212
Mathias <Storm>
2023-11-03 19:13:11 +0100

    feat: always redirect unauthenticated users to login screen

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,13 +9,12 @@ export const authConfig = {
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            if (isOnDashboard) {
-                if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
-            } else if (isLoggedIn) {
-                return Response.redirect(new URL('/dashboard', nextUrl));
+            const isOnLoginPage = nextUrl.pathname.startsWith('/login');
+
+            if(!isLoggedIn && !isOnLoginPage) {
+                return Response.redirect(new URL('/login', nextUrl))
             }
+
             return true;
         },
     },
