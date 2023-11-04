b5d8643c9ce10459bee4a7a819347f7a89a67810
Mathias Storm <mathias.storm@outlook.de>
2023-11-04 12:51:44 +0100

    feat: polish login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,8 @@
 import {signIn} from "@/lib/auth";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {} from "lucide-react";
+import {Icons} from "@/components/icons";
 
 export default function LoginPage() {
     const handleGoogleSignIn = async () => {
@@ -6,14 +10,21 @@ export default function LoginPage() {
         await signIn("google", {redirectTo: "/dashboard"});
     }
     return (
-        <main className="flex items-center justify-center md:h-screen">
-            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-                <form action={handleGoogleSignIn}>
-                    <button>
-                        Sign in with google
-                    </button>
-                </form>
-            </div>
+        <main className="flex items-center justify-center md:h-screen md:max-w-md mx-auto">
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Anmeldung</CardTitle>
+                    <CardDescription>Bitte melden Sie sich an, um die Anwendung zu nutzen. Derzeit wird lediglich Google als Authentifizierungsmethode angeboten.</CardDescription>
+                </CardHeader>
+                <CardContent>
+
+                    <form action={handleGoogleSignIn}>
+
+                        <Button><Icons.google className="mr-2 h-4 w-4" />Mit Google anmelden</Button>
+                    </form>
+                </CardContent>
+            </Card>
         </main>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import {db} from "@/lib/database";
 
 export default function Home() {
-  db
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
