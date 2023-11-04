a9cdec9c47a1d114804e0cec183a47aa18205dd1
Mathias Storm <mathias.storm@outlook.de>
2023-11-04 15:37:47 +0100

    feat: prettify ui layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
-import {auth} from "@/lib/auth";
+import { auth } from "@/lib/auth";
 
 export default async function Page() {
-    const x = await auth()
-    return <pre>{JSON.stringify(x)}</pre>
+  const x = await auth();
+  return (
+    <pre className="whitespace-pre-wrap">{JSON.stringify(x, null, 2)}</pre>
+  );
 }
