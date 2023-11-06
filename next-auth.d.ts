b5a3aa2cafebd8df9f9c612f6923a5dc29128c47
Mathias Storm <mathias.storm@outlook.de>
2023-11-06 17:57:22 +0100

    fix: typing of next-auth session object

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,9 @@
-import NextAuth from 'next-auth';
+import type { DefaultSession } from "next-auth";
 
-declare module 'next-auth' {
-    interface Session {
-        user: {
-            id: string;
-        } & DefaultSession['user'];
-    }
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
 }
