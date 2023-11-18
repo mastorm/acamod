148798719fdfada378265019487c98a63d6b0f15
Mathias Storm <mathias.storm@outlook.de>
2023-11-18 19:45:49 +0100

    update packages, refactor auth call

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import { auth } from "@/lib/auth";
+import { getSession } from "@/lib/getSession";
 
 export default async function Page() {
-  const x = await auth();
+  const x = await getSession();
   return (
     <pre className="whitespace-pre-wrap">{JSON.stringify(x, null, 2)}</pre>
   );
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,52 +1,48 @@
-import NextAuth, {NextAuthConfig} from 'next-auth';
-import {getEnvironment} from "@/lib/env";
-import {db} from "@/lib/database";
-import {DrizzleAdapter} from "@auth/drizzle-adapter";
+import NextAuth, { NextAuthConfig } from 'next-auth';
+import { getEnvironment } from "@/lib/env";
+import { db } from "@/lib/database";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GoogleProvider from "next-auth/providers/google"
+
 const env = getEnvironment();
 
-
 export const authConfig = {
-    providers: [
-        GoogleProvider({
-            clientId: env.GOOGLE_CLIENT_ID,
-            clientSecret: env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-    pages: {
-        signIn: '/login',
-    },
-    adapter: DrizzleAdapter(db),
-    callbacks: {
-        authorized({ auth, request: { nextUrl } }) {
-            const isLoggedIn = !!auth?.user;
-            const isOnLoginPage = nextUrl.pathname.startsWith('/login');
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  adapter: DrizzleAdapter(db),
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnLoginPage = nextUrl.pathname.startsWith('/login');
 
-            if(!isLoggedIn && !isOnLoginPage) {
-                return Response.redirect(new URL('/login', nextUrl))
-            }
+      if (!isLoggedIn && !isOnLoginPage) {
+        return Response.redirect(new URL('/login', nextUrl))
+      }
 
-            return true;
-        },
-        jwt({ token, account, user,  }) {
-            if (account) {
-                token.accessToken = account.access_token
-                token.id = user?.id
-            }
-            return token
-        },
-        session({ session, user}) {
-            if(session.user) {
-                session.user.id = user.id
-            }
-            return session;
-        },
+      return true;
     },
+    jwt({ token, account, user, }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.id = user?.id
+      }
+      return token
+    },
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+      }
+      return session;
+    },
+  },
 
 } satisfies NextAuthConfig;
 
-
 export const { auth, signIn, signOut, handlers } = NextAuth(authConfig);
-
-
-
