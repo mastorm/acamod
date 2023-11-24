9030750df0298f01c2bedfe346574cd2024a0051
Mathias Storm <mathias.storm@outlook.de>
2023-11-24 20:23:04 +0100

    require sessions explicitly

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,16 @@
 import { db } from "@/lib/database";
 import { CreateNewModuleCard } from "./create-new-module-card";
-import { getSession } from "@/lib/getSession";
+import { getRequiredSession } from "@/lib/getSession";
 import { modules } from "@/lib/schema";
 import { eq } from "drizzle-orm";
 
 export default async function Page() {
-  const session = await getSession();
+  const session = await getRequiredSession();
   const userModules = await db
     .select()
     .from(modules)
-    .where(eq(modules.userId, session?.user.id!));
+    .where(eq(modules.userId, session?.user.id));
+
   return (
     <main>
       <h1 className="text-2xl font-bold pb-6">Module</h1>
--- lib/getSession.tsx
+++ lib/getSession.tsx
@@ -1,4 +1,12 @@
 import { cache } from "react";
 import { auth } from "./auth";
 
-export const getSession = cache(async () => await auth())
+export const getSession = cache(async () => await auth());
+
+export const getRequiredSession = cache(async () => {
+  const session = await auth();
+  if (session == null) {
+    throw new Error("Expected session to be set but it wasnt");
+  }
+  return session;
+});
