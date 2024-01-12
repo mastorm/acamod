4bbc13f426647df929d65198c0306da487d05714
Mathias Storm <mathias.storm@outlook.de>
2024-01-12 21:11:29 +0100

    fix: type error

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -33,7 +33,7 @@ export default async function Page() {
           <ModuleCard
             key={x.modules.id}
             module={x.modules}
-            moduleUsage={x.moduleUsage}
+            moduleUsage={x.moduleUsages}
           />
         ))}
       </div>
