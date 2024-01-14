bf15381255b366411b50194a74b0a1a842f67004
Mathias Storm <mathias.storm@outlook.de>
2024-01-14 16:09:28 +0100

    feat: include owner in group members

--- lib/data/groups.ts
+++ lib/data/groups.ts
@@ -1,16 +1,33 @@
 import { eq } from "drizzle-orm";
 import { db } from "../database";
-import { groupMemberships, users } from "../schema";
+import { groupMemberships, groups, users } from "../schema";
 
 export async function getGroupMembers({ groupId }: { groupId: number }) {
-  return db
+  const owner = await db
     .select({
       id: users.id,
       name: users.name,
       image: users.image,
       email: users.email,
     })
+    .from(groups)
+    .where(eq(groups.id, groupId))
+    .leftJoin(users, eq(groups.userId, users.id));
+
+  const members = await db
+    .select({
+      id: users.id,
+      name: users.name,
+      image: users.image,
+      email: users.email,
+      hasAcceptedInvitation: groupMemberships.hasAcceptedInvitation,
+    })
     .from(groupMemberships)
     .leftJoin(users, eq(groupMemberships.userId, users.id))
     .where(eq(groupMemberships.groupId, groupId));
+
+  return [
+    { ...owner[0], hasAcceptedInvitation: true, isOwner: true },
+    ...members.map((x) => ({ ...x, isOwner: false })),
+  ];
 }
