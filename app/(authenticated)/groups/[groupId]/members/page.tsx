317ecf6a33a6bddfeed297bbed0ef6630400adf9
Mathias Storm <mathias.storm@outlook.de>
2024-01-14 11:46:40 +0100

    feat: show users of group

--- app/(authenticated)/groups/[groupId]/members/page.tsx
+++ app/(authenticated)/groups/[groupId]/members/page.tsx
@@ -1,3 +1,46 @@
-export default function GroupMembersPage() {
-  return <>lol</>;
+import { getGroupMembers } from "@/lib/data/groups";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Avatar } from "@/components/ui/avatar";
+import UserAvatar from "@/components/layout/user-avatar";
+
+export default async function GroupMembersPage({
+  params: { groupId },
+}: {
+  params: { groupId: string };
+}) {
+  const members = await getGroupMembers({ groupId: +groupId });
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]"></TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Rolle</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {members.map((mem) => (
+            <TableRow key={mem.id}>
+              <TableCell className="font-medium">
+                <UserAvatar imageUrl={mem.image} handle={mem.name} />
+              </TableCell>
+              <TableCell>{mem.name}</TableCell>
+              <TableCell>{mem.email}</TableCell>
+              <TableCell>Mitglied</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
--- lib/data/groups.ts
+++ lib/data/groups.ts
@@ -1,3 +1,16 @@
+import { eq } from "drizzle-orm";
 import { db } from "../database";
+import { groupMemberships, users } from "../schema";
 
-export async function getGroupMembers() {}
+export async function getGroupMembers({ groupId }: { groupId: number }) {
+  return db
+    .select({
+      id: users.id,
+      name: users.name,
+      image: users.image,
+      email: users.email,
+    })
+    .from(groupMemberships)
+    .leftJoin(users, eq(groupMemberships.userId, users.id))
+    .where(eq(groupMemberships.id, groupId));
+}
