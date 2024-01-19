eeabfe634f44b7dd418ba58c50e1691347905ae6
Mathias Storm <mathias.storm@outlook.de>
2024-01-19 20:47:51 +0100

    feat: polish module card on startpage

--- app/(authenticated)/dashboard/module-card.tsx
+++ app/(authenticated)/dashboard/module-card.tsx
@@ -9,35 +9,60 @@ import Link from "next/link";
 import { urls } from "@/lib/urls";
 import { moduleUsages } from "@/lib/schema";
 import { CheckCheckIcon, CheckIcon } from "lucide-react";
+import { cva } from "class-variance-authority";
 
 interface ModuleCardProps {
   module: {
     id: number;
     name: string;
+    shortCode: string;
   };
-  moduleUsage: Pick<typeof moduleUsages.$inferSelect, "completedDate"> | null;
+  moduleUsage: Pick<
+    typeof moduleUsages.$inferSelect,
+    "completedDate" | "targetDate"
+  > | null;
 }
 
-export function ModuleCard({
-  module: { id, name },
-  moduleUsage,
-}: ModuleCardProps) {
+const cardStyles = cva("hover:bg-accent ", {
+  variants: {
+    isCompleted: {
+      true: "bg-green-500/10",
+      false: "",
+    },
+  },
+});
+
+function moduleStatusText(usage: ModuleCardProps["moduleUsage"]) {
+  if (usage?.completedDate) {
+    return `Abgeschlossen am ${usage.completedDate.toLocaleDateString()}`;
+  } else if (usage?.targetDate) {
+    return `Ziel: ${usage.targetDate.toLocaleDateString()}`;
+  }
+  return "Noch kein Ziel gesetzt";
+}
+
+function moduleTitle(module: ModuleCardProps["module"]) {
+  return module.name;
+}
+
+export function ModuleCard({ module, moduleUsage }: ModuleCardProps) {
   return (
-    <Link href={urls.moduleDetails(id)}>
-      <Card className="hover:bg-accent bg-green-500/10">
+    <Link href={urls.moduleDetails(module.id)}>
+      <Card
+        className={cardStyles({
+          isCompleted: moduleUsage?.completedDate != null,
+        })}
+      >
         <CardHeader>
           <div className="flex gap-4">
-            <CardTitle>{name}</CardTitle>
+            <CardTitle>{moduleTitle(module)}</CardTitle>
             {moduleUsage?.completedDate && (
               <CheckCheckIcon className="text-green-500" />
             )}
           </div>
         </CardHeader>
         <CardContent>
-          <CardDescription>
-            Hier könnten später interessante Statistiken oder Infos zu Modulen
-            zu finden sein. Vorerst dient die Karte nur als link.
-          </CardDescription>
+          <CardDescription>{moduleStatusText(moduleUsage)}</CardDescription>
         </CardContent>
       </Card>
     </Link>
