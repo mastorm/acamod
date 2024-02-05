415e00071054431f03c7b1db437779a7b27476ce
Mathias Storm <mathias.storm@outlook.de>
2024-02-05 16:47:57 +0100

    refactor: clean up usings

--- app/(authenticated)/groups/[groupId]/page.tsx
+++ app/(authenticated)/groups/[groupId]/page.tsx
@@ -1,13 +1,5 @@
-import { db } from "@/lib/database";
-import { groups } from "@/lib/schema/groups";
-import { getRequiredSession } from "@/lib/getSession";
-import { and, eq } from "drizzle-orm";
-import { notFound, redirect } from "next/navigation";
-import { DetailLayout } from "@/components/layout/detail-layout";
-import { PlusCircleIcon, UserIcon } from "lucide-react";
-import { ActionButton } from "@/components/layout/action-button";
-import { EditGroupAction } from "@/app/(authenticated)/groups/[groupId]/edit-group-action";
 import { urls } from "@/lib/urls";
+import { redirect } from "next/navigation";
 
 interface GroupDetailsPageProps {
   params: {
